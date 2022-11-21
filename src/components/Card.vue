aae36eab9eba6eadf366429d5808a6a9a2fd3989
NolaReziyana <80933699+NolaReziyana@users.noreply.github.com>
2022-11-21 17:29:36 +0700

    digiasia

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,20 +8,54 @@
                 <h1 class="text-center text-xl font-bold text-black">What can you build?</h1>
             </div>
         </div>
-        <div class="flex flex-row mt-10 ml-10">
-            <div class="card h-200px w-50px bg-red-100 ">
+        <div class="flex flex-row mt-10 ml-10 mr-10">
+            <div class="card w-30 bg-red-100 ">
                 <div class="card-body">
                     <img src="@/assets/images/Asset Icon-1. Digital Wallet.png " alt="image" width="100px" height="100px">
-
-                    <h2 class="card-title text-xl font-bold text-rose-900">Digital Wallet</h2>
-                    <p>Digital Wallet You can embed our digital wallet into your native apps with account linkage to enhance your customer experience with seamless transaction journey to improve loyalty on your apps while we take care of the regulatory compliance and technology provision.</p>
-                    <div class="card-actions justify-start ">
+                    <h2 class="card-title text-xl font-bold text-rose-900 mt-5">Digital Wallet</h2>
+                    <p class="mt-2">Digital Wallet You can embed our digital wallet into your native apps with account linkage to enhance your customer experience with seamless transaction journey to improve loyalty on your apps while we take care of the regulatory compliance and technology provision.</p>
+                    <div class="card-actions justify-start mt-4">
                         <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
                         <img src="@/assets/images/right-arrow.png " alt="image" width="20px" height="20px" class="mt-1">
                     </div>
                 </div>
             </div>
-            
+            <div class="flex flex-col ml-4">
+                <div class="card w-50px bg-red-100 ">
+                    <div class="card-body">
+                        <div class="flex flex-1 flex-row justify-center">
+                            <div class="flex justify-start">
+                                <img src="@/assets/images/Asset Icon_2. Icon QRIS Payment.png " alt="image" width="100px" height="100px">
+                            </div>
+                            <div class="flex flex-col ml-7 ">
+                                <h2 class=" text-center card-title text-xl font-bold text-rose-900 ">QRIS Payment</h2>
+                                <p class="mt-2">Either for a payor or payment acceptor, with our embedded QRIS Payment as a Service you can enable payment by embedding our QRIS Payment in your customer or POS applications headache free.</p>
+                                <div class="card-actions justify-start mt-4">
+                                    <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
+                                    <img src="@/assets/images/right-arrow.png " alt="image" width="20px" height="20px" class="mt-1">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <div class="card w-50px bg-red-100 mt-4 ">
+                    <div class="card-body">
+                        <div class="flex flex-1 flex-row justify-center">
+                            <div class="flex justify-start">
+                                <img src="@/assets/images/Asset Icon_3. Icon Cash Management.png " alt="image" width="100px" height="100px">
+                            </div>
+                            <div class="flex flex-col ml-7 ">
+                                <h2 class="card-title text-xl font-bold text-rose-900">Cash Management</h2>
+                                <p class="mt-2">By Embedding our Cash Management system in your ERP system allows you to create close-loop cash management system for your value chain stake holders.</p>
+                                <div class="card-actions justify-start mt-4">
+                                    <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
+                                    <img src="@/assets/images/right-arrow.png " alt="image" width="20px" height="20px" class="mt-1">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>                 
+            </div>
         </div>
     </div>
 </template>
