f4fadace634692a2eaa8d69180d497a870723a66
NolaReziyana <80933699+NolaReziyana@users.noreply.github.com>
2022-11-21 14:45:45 +0700

    digiasia

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col mt-10">
+        <div class="flex flex-1 flex-col justify-center">
+            <div class="flex justify-center">
+                <img src="@/assets/images/LIST UNGU.png" alt="image" width="60px" height="30px"/>
+            </div>
+            <div class="flex flex-col mt-2 mr-12 ml-12">
+                <h1 class="text-center text-xl font-bold text-black">What can you build?</h1>
+            </div>
+        </div>
+        <div class="flex flex-row mt-10 ml-10">
+            <div class="card h-200px w-50px bg-red-100 ">
+                <div class="card-body">
+                    <img src="@/assets/images/Asset Icon-1. Digital Wallet.png " alt="image" width="100px" height="100px">
+
+                    <h2 class="card-title text-xl font-bold text-rose-900">Digital Wallet</h2>
+                    <p>Digital Wallet You can embed our digital wallet into your native apps with account linkage to enhance your customer experience with seamless transaction journey to improve loyalty on your apps while we take care of the regulatory compliance and technology provision.</p>
+                    <div class="card-actions justify-start ">
+                        <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
+                        <img src="@/assets/images/right-arrow.png " alt="image" width="20px" height="20px" class="mt-1">
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Card from "@/components/Card.vue";
+import Description from "@/components/Description.vue";
+import NavbarVue from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-col">
+      <NavbarVue />
+      <img src="@/assets/images/gambar.png" alt="image" class="w-full h-200px"/>
+      <Description />
+      <Card />
+    </div>
+  </main>
+</template>
