a960012e462706ac8132878fb9e6ff8270f29107
NolaReziyana <80933699+NolaReziyana@users.noreply.github.com>
2022-11-22 17:17:21 +0700

    digiasia

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,33 +1,74 @@
+<!-- 
+<script>
+     var cardlist = new Vue({
+        el: '#cardlist',
+        data: {
+            lists :[
+                {image:'@/assets/images/Foto/Foto 1.png', title:'Facility Management', description:'API for services that usage for facilities (apartment, office)'},
+                {image:'@/assets/images/Foto/Foto 2.png', title:'KasPro Ecosystem Supply Chain', description: 'API bundle for Business Ecosystem Solution that aligns Principa Distributors Wholesalers, Retailers into ore robust ecosystem & securing risks under one roof.'},
+                {image:'@/assets/images/Foto/Foto 3.png', title:'Retail Chain', description:'API bundle for retail chain business contains Customer onboarding, etc.'},
+                {image:'@/assets/images/Foto/Foto 4.png', title:'P2P', description:'Financial technology lending that gives lending, insta Imerts without credit cards, cell phone credit and PayLater services.'},
+                {image:'@/assets/images/Foto/Foto 5.png', title:'Delivery Service', description:'API for delivery services that provide customers onboarding, payment and wallet services.'},
+                {image:'@/assets/images/Foto/Foto 6.png', title:'Transportation', description:'Mobile-based transporting marketplace that allows users to find and book for taxi bike, taxi, and on demand delivery.'},
+                {image:'@/assets/images/Foto/Foto 7.png', title:'Biller Management', description:'Customers can explore & embedded each or all of the list of our biller products.'},
+                {image:'@/assets/images/Foto/Foto 8.png', title:'QRIS Merchant Aggregator', description:'API bundle for business that need Acquiring merchant and provide QRIS feature requirements.'},
+                {image:'@/assets/images/Foto/Foto 9.png', title:'Integrated Cash Management', description:'A tailor-made cash management for corporate customers aiming.to enhance their corporate efficiency.'},
+                {image:'@/assets/images/Foto/Foto 10.png', title:'Account Linkage', description:'Linkage accounts and system integration in your application or business, electronic services can already be used.'},
+                {image:'@/assets/images/Foto/Foto 11.png', title:'Kaspro Bank', description:'Digitization of Traditional Cooperatives-through secure and seamless platform and technology tie-up.'},
+            ]
+        }
+     })
+</script> -->
 
 <template>
-    <div class="flex flex-row mt-10">
-        <div class="flex flex-1 flex-col justify-center mt-9 ml-12">
-            <div class="flex justify-center">
-            <img src="@/assets/images/LIST UNGU.png" alt="image" width="60px" height="30px"/>
-            </div>
-            <div class="flex flex-col mt-2 mr-12 ml-12">
-            <h1 class="text-center text-xl font-bold text-black">Use Cases</h1>
-            </div>
-        </div>
-        <div class="flex justify-end">
-            <img src="@/assets/images/Ornamen Bulat Tengah.png" alt="image" width="60px" height="50px">
-        </div>
+  <div class="flex flex-row mt-10">
+    <div class="flex flex-1 flex-col justify-center mt-9 ml-12">
+      <div class="flex justify-center">
+        <img
+          src="@/assets/images/LIST UNGU.png"
+          alt="image"
+          width="60px"
+          height="30px"
+        />
+      </div>
+      <div class="flex flex-col mt-2 mr-12 ml-12">
+        <h1 class="text-center text-xl font-bold text-black">Use Cases</h1>
+      </div>
     </div>
-    <div class="flex-1 grid grid-cols-3 ml-10 mr-10 mt-10">
-        <ul id="cardlist">
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img src="@/assets/images/Foto/Foto 1.png" alt="Shoes" class="rounded-xl" />
-                </figure>
-                <div class="card-body items-start text-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-start mt-4">
-                        <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
-                        <img src="@/assets/images/right-arrow.png " alt="image" width="20px" height="20px" class="mt-1">
-                    </div>
-                </div>
-            </div>
-        </ul>
+    <div class="flex justify-end">
+      <img
+        src="@/assets/images/Ornamen Bulat Tengah.png"
+        alt="image"
+        width="60px"
+        height="50px"
+      />
     </div>
+  </div>
+  <div class="flex-1 grid grid-cols-3 ml-10 mr-10 mt-10">
+    <ul id="cardlist">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="@/assets/images/Foto/Foto 1.png"
+            alt="Shoes"
+            class="rounded-xl"
+          />
+        </figure>
+        <div class="card-body items-start text-start">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-start mt-4">
+            <h2 class="text-l font-semibold text-rose-900">View Journey</h2>
+            <img
+              src="@/assets/images/right-arrow.png "
+              alt="image"
+              width="20px"
+              height="20px"
+              class="mt-1"
+            />
+          </div>
+        </div>
+      </div>
+    </ul>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import Card from "@/components/Card.vue";
+import CardBuild from "@/components/CardBuild.vue";
 import CardList from "@/components/CardList.vue";
-import Description from "@/components/Description.vue";
-import NavbarVue from "@/components/Navbar.vue";
+import DescriptionSection from "@/components/DescriptionSection.vue";
+import NavbarSection from "@/components/NavbarSection.vue";
 </script>
 
 <template>
   <main>
     <div class="flex flex-col">
-      <NavbarVue />
-      <img src="@/assets/images/gambar.png" alt="image" class="w-full h-200px"/>
-      <Description />
-      <Card />
+      <NavbarSection />
+      <img
+        src="@/assets/images/gambar.png"
+        alt="image"
+        class="w-full h-200px"
+      />
+      <DescriptionSection />
+      <CardBuild />
       <CardList />
     </div>
   </main>
